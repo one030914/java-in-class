c8a3f5ce4c277109d811726f980d7eca32a6d265
GarryAdam <105182656+one030914@users.noreply.github.com>
2024-11-28 01:41:05 +0800

    Add files via upload

--- exam/ex19.java
+++ exam/ex19.java
@@ -1,29 +1,42 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ex19{
     public static void main(String[] args){
         int[] arr1 = {1,2,3,4,5}, arr2 = {1,3,6}, arr3 = {2,5,7,9,10,11,12}, arr4 = {2,3,5,6,10,11};
 
         set(arr1, arr2);
-        for(int i = 0; i < result.length; i++){
-            System.out.println(result[i]);
-        }
+        set(arr4, arr4);
     }
     public static void set(int[] arr1, int[] arr2){
-        ArrayList<Int> result = new ArrayList<Int>;
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        int i = 0, j = 0;
 
-        for(int i = 0; i < arr1.length; i++){
-            result.add(arr1[i]);
-        }
-        for(int i = 0; i < result.size; i++){
-            int index = 0;
-            if(arr2[index] != result[i]){
-                if(arr[index] < result[i]){
-                    
-                }
+        while(i < arr1.length && j < arr2.length){
+            if(arr1[i] < arr2[j]){
+                result.add(arr1[i++]);
+            }else if(arr1[i] > arr2[j]){
+                result.add(arr2[j++]);
+            }else{
+                result.add(arr1[i++]);
+                j++;
             }
-            index++;
+        }
+        while (i < arr1.length) {
+            if (!result.contains(arr1[i])) {
+                result.add(arr1[i]);
+            }
+            i++;
+        }
+        while (j < arr2.length) {
+            if (!result.contains(arr2[j])) {
+                result.add(arr2[j]);
+            }
+            j++;
         }
         
+        System.out.println("arr1 = " + Arrays.toString(arr1));
+        System.out.println("arr2 = " + Arrays.toString(arr2));
+        System.out.println(result.toString());
     }
 }
