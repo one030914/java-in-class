0ff22be8477e3955d4222785736619c2a47fb790
DESKTOP-F0GH6SG\USER <DESKTOP-F0GH6SG\USER>
2024-12-10 16:01:29 +0800

    java

---
+++ exam/ex23/ex23.java
@@ -0,0 +1,84 @@
+public class ex23{
+    public static void main(String[] args){
+        Chicken chicken1 = new Chicken("CA", 1, 2);
+        Chicken chicken2 = new Chicken("CB", 2, 2);
+        Rabbit rabbit1 = new Rabbit("RA", 3, 4);
+        Rabbit rabbit2 = new Rabbit("RA", 4, 4);
+        Dog dog = new Dog("DA", 5, 4);
+
+        chicken1.AnimalInfo();
+        chicken2.AnimalInfo();
+        rabbit1.AnimalInfo();
+        rabbit2.AnimalInfo();
+        dog.AnimalInfo();
+
+        int total = 0, avg = 0;
+        total += chicken1.getFoots();
+        total += chicken2.getFoots();
+        total += rabbit1.getFoots();
+        total += rabbit2.getFoots();
+        total += dog.getFoots();
+        avg = total / 5;
+        System.out.println("Total foots: " + total + ", avg age: " + avg);
+    }
+}
+
+class Animal{
+    String name = "";
+    int age = 0;
+    public Animal(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public void AnimalInfo(){
+        System.out.printf("Animal: %s, Age: %d\n", this.name, this.age);
+    }
+    public int getAge(){
+        return this.age;
+    }
+}
+
+class Chicken extends Animal{
+    private int foots = 0;
+    public Chicken(String name, int age, int foots){
+        super(name, age);
+        this.foots = foots;
+    }
+    @Override
+    public void AnimalInfo(){
+        System.out.printf("Chicken: %s, Age: %d, foots: %d\n", this.name, this.age, this.foots);
+    }
+    public int getFoots(){
+        return this.foots;
+    }
+}
+
+class Rabbit extends Animal{
+    private int foots = 0;
+    public Rabbit(String name, int age, int foots){
+        super(name, age);
+        this.foots = foots;
+    }
+    @Override
+    public void AnimalInfo(){
+        System.out.printf("Rabbit: %s, Age: %d, foots: %d\n", this.name, this.age, this.foots);
+    }
+    public int getFoots(){
+        return this.foots;
+    }
+}
+
+class Dog extends Animal{
+    private int foots = 0;
+    public Dog(String name, int age, int foots){
+        super(name, age);
+        this.foots = foots;
+    }
+    @Override
+    public void AnimalInfo(){
+        System.out.printf("Dog: %s, Age: %d, foots: %d\n", this.name, this.age, this.foots);
+    }
+    public int getFoots(){
+        return this.foots;
+    }
+}
